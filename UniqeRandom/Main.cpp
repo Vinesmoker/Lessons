c7940d2f0a4914856a4b8cb2046f2d7803c8da98
Vinok_yt86@top-academy.ru <igorvinokurov1984@gmail.com>
2022-11-17 21:19:09 +0300

    Test with Uniq

--- UniqeRandom/Main.cpp
+++ UniqeRandom/Main.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 using namespace std;
 
-void main()
+void main ()
 {
 	setlocale(LC_ALL, "");
 	const int n = 10; int minLim; int maxLim;
@@ -18,36 +18,33 @@ void main()
 		cout << arr[i] << "\t";
 	}
 	cout << "\n\n";
-	cout << "Sort the array in ascending order:" << endl;
+	
+	cout << "Filling the array with unique numbers:" << endl;
 	for (int i = 0; i < n; i++)
 	{
-		int buff = 0;
-		for (int j = i + 1; j < n; j++)
+		bool ran;
+		for (int j = 0; j < n; j++)
 		{
-			if (arr[j] < arr[i])
+			ran = true;
+			for (int r = 0; r < n; r++)
 			{
-				int buff = arr[i];
-				arr[i] = arr[j];
-				arr[j] = buff;
+				for (int c = 0; c < n; c++)
+				{
+					if (r == i && c == j)
+					{
+						r = c = n;
+						continue;
+					}
+					if (arr[r][c] == arr[i][j])
+					{
+
+					}
+				}
 			}
 		}
-		cout << arr[i] << "\t";
 	}
-	cout << "\n\n";
-	cout << "Sort the array in descending order:" << endl;
 	for (int i = 0; i < n; i++)
 	{
-		int buff = 0;
-		for (int j = i + 1; j < n; j++)
-		{
-			if (arr[j] > arr[i])
-			{
-				buff = arr[j];
-				arr[j] = arr[i];
-				arr[i] = buff;
-			}
-		}
 		cout << arr[i] << "\t";
 	}
-	cout << "\n\n";
 }
