023f1464a1ab4258599b6b929c7ecd62d8149990
Vinok_yt86@top-academy.ru <igorvinokurov1984@gmail.com>
2022-11-26 13:47:52 +0300

    Done

--- UniqeRandom/Main.cpp
+++ UniqeRandom/Main.cpp
@@ -7,42 +7,32 @@ void main ()
 	const int n = 10; int minLim; int maxLim;
 	int arr[n]{};
 	cout << "Enter the minimum value of the number range: "; cin >> minLim;
-	cout << "Enter the maximum value of the number range: "; cin >> maxLim;
-	for (int i = 0; i < n; i++)
-	{
-		arr[i] = minLim + rand() % (maxLim - minLim) + 1;
-	}
 	cout << endl;
-	for (int i = 0; i < n; i++)
-	{
-		cout << arr[i] << "\t";
-	}
-	cout << "\n\n";
-	
-	cout << "Filling the array with unique numbers:" << endl;
-	for (int i = 0; i < n; i++)
-	{
-		bool ran;
-		for (int j = 0; j < n; j++)
-		{
-			ran = true;
-			for (int r = 0; r < n; r++)
-			{
-				for (int c = 0; c < n; c++)
-				{
-					if (r == i && c == j)
-					{
-						r = c = n;
-						continue;
-					}
-					if (arr[r][c] == arr[i][j])
-					{
+	cout << "Enter the maximum value of the number range: "; cin >> maxLim;
+	cout << endl;
 
-					}
-				}
+	bool random;
+	for (int i = 0; i < n;)
+	{
+		random = false;
+		int ranVal = minLim + rand() % (maxLim - minLim);
+		for (int j = 0; j < i; j++)
+		{
+			if (arr[j] == ranVal)
+			{
+				random = true; 
+				break;
 			}
 		}
+		if (random == false)
+		{
+			arr[i] = ranVal; i++;
+		}
 	}
+
+
+
+
 	for (int i = 0; i < n; i++)
 	{
 		cout << arr[i] << "\t";
